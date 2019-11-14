8c843239f9818be6be8cb4f62eb6949594ce8ddb
Marina Vieira Ferreira <marina.ferreira@pampa.compasso>
2019-11-14 13:22:18 -0300

    Desafio Jaison

---
+++ js/animacaobotao.js
@@ -0,0 +1,5 @@
+$('.botao').on('click', function(e){
+    e.preventDefault();
+    $('.botao1').removeClass("invisivel");
+    $('.botao2').removeClass("invisivel");
+});
---
+++ js/function.js
@@ -0,0 +1,84 @@
+$(function () {
+    var buscar = $('.js-input-usuario');
+    var retorno = $("#retorno");
+    var form = $(".js-form-busca");
+    var botao = $ (".botao");
+    var botao1 = $(".botao1");
+    var botao2 = $(".botao2");
+    
+    //form.on('submit', false);
+    form.on('submit', function(event){
+        event.preventDefault();
+        return false;
+    });
+    botao.on('click', function () {
+        $('#parabens').removeClass("invisivel");
+        $.ajax({
+            url: "https://api.github.com/users/" + buscar.val()
+        }).done(function (data) {
+            console.log(data);
+
+            var texto = "<h3 class='descricao'><b>" + data.name + "</b></h3>"
+            texto += "<br /><img class='icone' src='" + data.avatar_url + "' alt='" + data.name + "' />"
+            texto += `<br /><div class='descricao'><b>Local de trabalho:</b> ${data.location} </div>`
+            texto += `<br /><div class='descricao'><b>Número de repositórios:</b> ${data.public_repos}</div>`
+            texto += `<br /><div class='descricao'><b>Número de Seguidores:</b> ${data.following} </div>`
+            
+            retorno.html(texto);
+            getrepos(texto, buscar)
+            getvisitados(texto, buscar)
+
+
+        }).error(function () {
+            retorno.text('Usuario não encontrado')
+        }).always(function () {
+            buscar.fadeIn();
+        });
+    });
+          function getrepos(texto, buscar) {
+            
+                botao1.on('click', function () {
+                $.ajax({
+                    url: "https://api.github.com/users/" + buscar.val() + "/repos"
+                }).done(function (data) {
+                    console.log(data);
+    
+                    $.each(data, function (key, item) {
+                        console.log(item);
+    
+                        texto += "<br/><a class='links' href='" + item.html_url + "'>" + item.name + "</a>"
+                    });
+                    retorno.html(texto);
+
+                }).error(function () {
+                    retorno.text('Esse usuário não possui repositórios')
+                }).always(function () {
+                    buscar.fadeIn();
+                });
+            });
+        }
+            function getvisitados(texto, buscar) {
+               
+            botao2.on('click', function () {
+            $.ajax({
+                url: "https://api.github.com/users/" + buscar.val() + "/starred"
+            }).done(function (data) {
+                console.log(data);
+
+               $.each(data, function (key, item) {
+                    console.log(item);
+
+                   texto += "<br/><a class='links' href='" + item.clone_url + "'>" + item.full_name + "</a>"
+                });
+
+                retorno.html(texto);
+
+            }).error(function () {
+                retorno.text('Não encontrado repositórios mais visitados')
+            }).always(function () {
+                buscar.fadeIn();
+            });
+        });
+    }
+});
+
